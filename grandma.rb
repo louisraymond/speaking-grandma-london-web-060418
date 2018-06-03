f050f9be207cca1c52cec54c61789b110afee73c
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-06-03 18:20:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,11 @@
 
 def speak_to_grandma(phrase)
- if 
-    return true
+ 
+ 
+ if dog == "hungry"
+  puts "Refilling food bowl."
+elsif dog == "thirsty"
+  puts "Refilling water bowl."
+else
+  puts "Reading newspaper."
 end
-  
